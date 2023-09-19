1251b2f318053a02b65dbc9862f68216a63b1187
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2023-09-19 18:59:35 +0300

    Added authorization handler for bot API, bug fixes

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+from asyncio.exceptions import CancelledError
+from fastapi.concurrency import asynccontextmanager
 import uvicorn
 from fastapi import APIRouter, FastAPI, Request
 from disnake.ext import commands
@@ -13,16 +15,29 @@ class APIService(FastAPI):
 router = APIRouter()
 
 
+@asynccontextmanager
+async def lifespan(app: APIService):
+    yield
+    try:
+        await app.bot.close()
+    except CancelledError:
+        pass
+    app.bot.loop.stop()
+
+
 @router.post("/bot-api/send-notify")
-async def index(request: Request):
+async def index(request: Request, message: str):
     bot: commands.Bot = request.app.bot
-    ch = bot.get_channel(MAKERS_CHAT_ID)
-    await ch.send("Success")
-    return {"channel": ch.name}
+    channel = bot.get_channel(MAKERS_CHAT_ID)
+    try:
+        await channel.send(content=message)
+        return {"status": "ok", "message": f"{message}"}
+    except Exception as error:
+        return {"status": "error", "message": error}
 
 
 def make_app(bot):
-    app = APIService(bot)
+    app = APIService(bot, lifespan=lifespan)
     app.include_router(router)
     return app
 
@@ -31,10 +46,7 @@ async def start_server(bot: commands.Bot) -> None:
     app = make_app(bot)
 
     config = uvicorn.Config(
-        app,
-        host="0.0.0.0",
-        port=8080,  # or whatever
-        lifespan="on",
+        app, host="0.0.0.0", port=8080, lifespan="on", reload=True, reload_delay=1.0
     )
 
     server = uvicorn.Server(config)
--- config.py
+++ config.py
@@ -5,15 +5,21 @@ from dotenv import load_dotenv
 load_dotenv("./venv/.env")
 
 PREFIX = "/"
+
 TOKEN = getenv("TOKEN")
+
 MYSQL_USER = getenv("MYSQL_USER")
 MYSQL_PASSWORD = getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = getenv("MYSQL_DATABASE")
 MYSQL_HOST = getenv("MYSQL_HOST")
 MYSQL_PORT = getenv("MYSQL_PORT")
-CHIEF_ROLE_ID = 1040606231489953823
-MAKER_ROLE_ID = 885145513949802568
-MAKERS_CHAT_ID = 804644436613005372
+
+CHIEF_ROLE_ID = int(getenv("CHIEF_ROLE_ID"))
+MAKER_ROLE_ID = int(getenv("MAKER_ROLE_ID"))
+MAKERS_CHAT_ID = int(getenv("MAKERS_CHAT_ID"))
+
+JWT_SECRET = getenv("JWT_SECRET")
+JWT_ALGORITHM = getenv("JWT_ALGORITHM")
 
 
 # https://discord.com/api/oauth2/authorize?client_id=1089657230330183765&permissions=274878237888&scope=applications.commands%20bot
