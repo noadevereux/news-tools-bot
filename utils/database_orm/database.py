29455515d1f61509d87d83c90fa1f02fd149d9c2
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2023-10-06 07:43:45 +0300

    Set pool recycle less than mysql wait timeout

--- utils/database_orm/database.py
+++ utils/database_orm/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from config import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_HOST, MYSQL_PORT
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=20, max_overflow=10)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=21_600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base: DeclarativeBase = declarative_base()
