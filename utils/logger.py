1d42799380568017db84fc03085617dad067f7f7
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2023-10-14 04:41:49 +0300

    Fixed typehint for log message

--- utils/logger.py
+++ utils/logger.py
@@ -5,12 +5,13 @@
 from aiofiles import open as aiopen
 from datetime import datetime
 
+
 class Logger:
 
     def __init__(self, file: str) -> None:
         self.file = f"./logs/{file}"
 
-    async def info(self, log: str):
+    async def info(self, log: str | Exception):
         """
         Writes an info message into the log file.
 
@@ -20,11 +21,11 @@ class Logger:
             Info message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        async with aiopen(file = self.file, mode = "a", encoding = "utf-8") as file:
+        async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"{date_time} - INFO {log}\n")
         print(log + "\n")
 
-    async def warning(self, log: str):
+    async def warning(self, log: str | Exception):
         """
         Writes a warning message into the log file.
 
@@ -34,11 +35,11 @@ class Logger:
             Warning message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        async with aiopen(file = self.file, mode = "a", encoding = "utf-8") as file:
+        async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"{date_time} - WARNING {log}\n")
         print(log + "\n")
-    
-    async def error(self, log: str):
+
+    async def error(self, log: str | Exception):
         """
         Writes an error message into the log file.
 
@@ -48,11 +49,11 @@ class Logger:
             Error message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        async with aiopen(file = self.file, mode = "a", encoding = "utf-8") as file:
+        async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"{date_time} - ERROR {log}\n")
         print(log + "\n")
 
-    async def critical(self, log: str):
+    async def critical(self, log: str | Exception):
         """
         Writes a critical error message into the log file.
 
@@ -62,6 +63,6 @@ class Logger:
             Critical error message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        async with aiopen(file = self.file, mode = "a", encoding = "utf-8") as file:
+        async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"{date_time} - CRITICAL ERROR {log}\n")
         print(log + "\n")
