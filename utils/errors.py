88aa8e8384d142dbf45b8368457dcf0d667dde15
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2023-10-22 00:47:46 +0300

    Remove errors.py

--- utils/errors.py
+++
@@ -1,8 +0,0 @@
-class IncorrectCommandName(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-
-class NotFound(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
