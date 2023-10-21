9148ea71b6c86dde3cd497d68dfda72e6243e94b
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2023-10-21 18:51:46 +0300

    Bug fix

--- ext/logger.py
+++ ext/logger.py
@@ -30,7 +30,7 @@ class Logger:
             Info message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        uid = self.generate_uid()
+        uid = await self.generate_uid()
         async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"[UID: {uid} ] {date_time} - INFO {log}\n")
         print(log + "\n")
@@ -46,7 +46,7 @@ class Logger:
             Warning message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        uid = self.generate_uid()
+        uid = await self.generate_uid()
         async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"[UID: {uid} ] {date_time} - WARNING {log}\n")
         print(log + "\n")
@@ -62,7 +62,7 @@ class Logger:
             Error message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        uid = self.generate_uid()
+        uid = await self.generate_uid()
         async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"[UID: {uid} ] {date_time} - ERROR {log}\n")
         print(log + "\n")
@@ -78,7 +78,7 @@ class Logger:
             Critical error message
         """
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        uid = self.generate_uid()
+        uid = await self.generate_uid()
         async with aiopen(file=self.file, mode="a", encoding="utf-8") as file:
             await file.write(f"[UID: {uid} ] {date_time} - CRITICAL ERROR {log}\n")
         print(log + "\n")
