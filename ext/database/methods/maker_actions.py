b0dd047c2afc0fd1dd5d7f9a3fb4b2a348ee9f39
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2023-12-17 22:09:25 +0300

    BOT-12: Make neccesary changes in maker actions methods

--- ext/database/methods/maker_actions.py
+++ ext/database/methods/maker_actions.py
@@ -20,6 +20,8 @@ async def add_maker_action(
             "setdate",
             "warn",
             "unwarn",
+            "pred",
+            "unpred",
         ],
         meta: str = None,
         reason: str = None,
